16263acae54897badc5f377c81a4dc8bad3429c7
Josie DeRosier <s209055@students.aie.edu>
2021-12-10 17:41:25 -0800

    Building map and adding scripts from test kitchen.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    int curSpeed;
+    public int walkSpeed;
+    public int runSpeed;
+
+    //public bool hasJumped;
+    //public float jumpForce;
+    // could implement a double jump if wanted
+
+    public Rigidbody2D rbody;
+
+    void Start()
+    {
+        curSpeed = walkSpeed;
+        //hasJumped = false;
+    }
+
+    private void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            curSpeed = runSpeed;
+        }
+        else
+        {
+            curSpeed = walkSpeed;
+        }
+
+        //Vector3 tempVel = Vector3.zero;
+        Vector2 tempVel = Vector2.zero;
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            tempVel.y += curSpeed;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            tempVel.x -= curSpeed;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            tempVel.y -= curSpeed;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            tempVel.x += curSpeed;
+        }
+
+        rbody.velocity = tempVel;
+    }
+
+    void Update()
+    {
+        //if (Input.GetKey(KeyCode.Space) && !hasJumped)
+        //{
+        //    rbody.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        //    hasJumped = true;
+        //}
+        //if(rbody.velocity.y == 0)
+        //{
+        //    hasJumped = false;
+        //}
+    }
+}
---
+++ Assets/Scripts/TimedAspect.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimedAspect : MonoBehaviour
+{
+    public float currentCyclePercent;   // the current percentage of how far through the cycle the aspect is (0 = start, 1 = end)
+
+    public bool cycleCompleted;         // has the cycle completed? on a back and forth, this will shift between true and false when the cycle turns over
+    [Tooltip("How many hours until a cycle change")]
+    public float cycleSpeed;            // how many hours until a cycle change
+
+    public bool hasSlider;              // will this aspect use a UI slider?
+    public Slider currentPercentSlider; // Unity UI slider to show the current percentage of the cycle completed (will go up and down on a back and forth aspect)
+
+    public NaughtyComponent dropdown;
+    public int cycleType;       // 0 is no cycle, 1 is Go Until, 2 is Back and Forth
+
+    void Start()
+    {
+        currentCyclePercent = 0;
+        cycleCompleted = false;
+        // dropdown.stringValue = "Go Until";
+    }
+
+    private void Update()
+    {
+        switch (dropdown.stringValue)
+        {
+            case "Go Until":
+                cycleType = 1;
+                break;
+            case "Back and Forth":
+                cycleType = 2;
+                break;
+            default:
+                break;
+        }
+    }
+}
