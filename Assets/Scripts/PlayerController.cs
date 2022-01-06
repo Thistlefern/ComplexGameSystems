e86f9aff635392f40ae8d1ea1369497e7afb5a0d
Josie DeRosier <s209055@students.aie.edu>
2022-01-06 15:24:52 -0800

    Remade the whole damn map yesterday/today, added player from Kenney. About to try out the new input system, wish current me luck!

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,69 +1,206 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerController : MonoBehaviour
 {
-    int curSpeed;
+    public int currentSpeed;
     public int walkSpeed;
     public int runSpeed;
+    public Rigidbody rbody;
+    public float jumpHeight;
+    public Animator animator;
+    public bool gameOver;
+    //public bool gameStarted;
 
-    //public bool hasJumped;
-    //public float jumpForce;
-    // could implement a double jump if wanted
+    public bool isMoving;
+    public bool isRunning;
+    public bool isFalling;
+    public float stamina;
+    readonly int staminaMax = 5;
 
-    public Rigidbody2D rbody;
+    public bool grounded;
+
+    public float timer;
 
     void Start()
     {
-        curSpeed = walkSpeed;
-        //hasJumped = false;
+        //gameStarted = false;
+
+        isFalling = false;
+        stamina = staminaMax;
+
+        grounded = true;
     }
 
-    private void FixedUpdate()
+    private void OnCollisionEnter(Collision collision)
+    {   
+        // TODO adjust to fit current game
+
+        //if (rbody.velocity.y < Mathf.Abs(0.01f))
+        //{
+        //    grounded = true;
+        //    isFalling = false;
+        //    if (collision.collider.name != "Plane")
+        //    {
+        //        audio.source.PlayOneShot(audio.sounds[2]);
+        //    }
+        //    else
+        //    {
+        //        audio.source.PlayOneShot(audio.sounds[0]);
+        //    }
+        //}
+    }
+
+    void FixedUpdate()
     {
-        if (Input.GetKey(KeyCode.LeftShift))
+        if (!gameOver)  // TODO && gameStarted
         {
-            curSpeed = runSpeed;
+            Vector3 tVel = Vector3.zero;
+            float tempY = rbody.velocity.y;
+
+            if (Input.GetKey(KeyCode.LeftShift) && isMoving && stamina != 0)
+            {
+                isRunning = true;
+                currentSpeed = runSpeed;
+                animator.SetBool("IsRunning", true);
+            }
+            else
+            {
+                isRunning = false;
+                currentSpeed = walkSpeed;
+                animator.SetBool("IsRunning", false);
+            }
+
+            if (Input.GetKey(KeyCode.W))
+            {
+                tVel += transform.forward;
+            }
+            if (Input.GetKey(KeyCode.S))
+            {
+                tVel -= transform.forward;
+            }
+
+            //if (Input.GetKey(KeyCode.W))
+            //{
+            //    tVel += transform.forward;
+            //    isMoving = true;
+            //    if (Input.GetKey(KeyCode.A))
+            //    {
+            //        transform.rotation = Quaternion.Euler(rotNW);
+            //    }
+            //    else if (Input.GetKey(KeyCode.D))
+            //    {
+            //        transform.rotation = Quaternion.Euler(rotNE);
+            //    }
+            //    else
+            //    {
+            //        transform.rotation = Quaternion.Euler(rotN);
+            //    }
+            //}
+            //if (Input.GetKey(KeyCode.S))
+            //{
+            //    tVel += transform.forward;
+            //    isMoving = true;
+            //    if (Input.GetKey(KeyCode.A))
+            //    {
+            //        transform.rotation = Quaternion.Euler(rotSW);
+            //    }
+            //    else if (Input.GetKey(KeyCode.D))
+            //    {
+            //        transform.rotation = Quaternion.Euler(rotSE);
+            //    }
+            //    else
+            //    {
+            //        transform.rotation = Quaternion.Euler(rotS);
+            //    }
+            //}
+            //if (Input.GetKey(KeyCode.A))
+            //{
+            //    isMoving = true;
+            //    if (!Input.GetKey(KeyCode.W) && !Input.GetKey(KeyCode.S))
+            //    {
+            //        tVel += transform.forward;
+            //        transform.rotation = Quaternion.Euler(rotW);
+            //    }
+            //}
+            //if (Input.GetKey(KeyCode.D))
+            //{
+            //    isMoving = true;
+            //    if (!Input.GetKey(KeyCode.W) && !Input.GetKey(KeyCode.S))
+            //    {
+            //        tVel += transform.forward;
+            //        transform.rotation = Quaternion.Euler(rotE);
+            //    }
+            //}
+            //if (Input.GetKey(KeyCode.Space) && grounded)
+            //{
+            //    isMoving = false;
+            //    rbody.AddForce(transform.up * jumpHeight, ForceMode.Impulse);
+            //    grounded = false;
+            //}
+
+            if (!Input.GetKey(KeyCode.W) && !Input.GetKey(KeyCode.A) && !Input.GetKey(KeyCode.S) && !Input.GetKey(KeyCode.D))
+            {
+                isMoving = false;
+            }
+
+
+            if (isMoving)
+            {
+                animator.SetBool("IsWalking", true);
+            }
+            else
+            {
+                animator.SetBool("IsWalking", false);
+            }
+
+            if (rbody.velocity.y < 0)
+            {
+                isFalling = true;
+            }
+
+            tVel = tVel.normalized * currentSpeed;
+            tVel.y = tempY;
+            rbody.velocity = tVel;
         }
         else
         {
-            curSpeed = walkSpeed;
+            rbody.velocity = new Vector3(0, 0);
+            animator.SetBool("IsWalking", false);
+            animator.SetBool("IsRunning", false);
+            isMoving = false;
         }
-
-        //Vector3 tempVel = Vector3.zero;
-        Vector2 tempVel = Vector2.zero;
-
-        if (Input.GetKey(KeyCode.W))
-        {
-            tempVel.y += curSpeed;
-        }
-        if (Input.GetKey(KeyCode.A))
-        {
-            tempVel.x -= curSpeed;
-        }
-        if (Input.GetKey(KeyCode.S))
-        {
-            tempVel.y -= curSpeed;
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            tempVel.x += curSpeed;
-        }
-
-        rbody.velocity = tempVel;
     }
 
-    void Update()
+    private void Update()
     {
-        //if (Input.GetKey(KeyCode.Space) && !hasJumped)
-        //{
-        //    rbody.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
-        //    hasJumped = true;
-        //}
-        //if(rbody.velocity.y == 0)
-        //{
-        //    hasJumped = false;
-        //}
+        if (!gameOver)
+        {
+            // TODO win state/scene loader here if needed
+
+            timer += Time.deltaTime;
+
+            if (isRunning)
+            {
+                stamina -= Time.deltaTime;
+                if (stamina < 0)
+                {
+                    stamina = 0;
+                }
+            }
+            else
+            {
+                if (!Input.GetKey(KeyCode.LeftShift))
+                {
+                    stamina += Time.deltaTime;
+                    if (stamina > staminaMax)
+                    {
+                        stamina = staminaMax;
+                    }
+                }
+            }
+        }
     }
 }
