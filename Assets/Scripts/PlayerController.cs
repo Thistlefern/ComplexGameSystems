6d44793d98958ca0bb4aebe71e38c1b198685bf4
Josie DeRosier <s209055@students.aie.edu>
2022-01-07 10:38:18 -0800

    About to add a shitload of colliders, here goes nothing

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,205 +2,176 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Interactions;
 
 public class PlayerController : MonoBehaviour
 {
-    public int currentSpeed;
-    public int walkSpeed;
-    public int runSpeed;
-    public Rigidbody rbody;
+    public float moveSpeed;
+    public float rotateSpeed;
     public float jumpHeight;
+    //public float burstSpeed;
+    //public GameObject projectile;
+
+    //private bool m_Charging;
+    private Vector2 m_Rotation;
+    //private Vector2 m_Look;
+    private Vector2 m_Move;
+    //private Vector2 m_Jump;
+
+    public bool hasJumped;
+    public float storedRot;
+
+    public Rigidbody rbody;
     public Animator animator;
-    public bool gameOver;
-    //public bool gameStarted;
 
-    public bool isMoving;
-    public bool isRunning;
-    public bool isFalling;
-    public float stamina;
-    readonly int staminaMax = 5;
-
-    public bool grounded;
-
-    public float timer;
-
-    void Start()
+    public void OnMove(InputAction.CallbackContext context)
     {
-        //gameStarted = false;
-
-        isFalling = false;
-        stamina = staminaMax;
-
-        grounded = true;
+        m_Move = context.ReadValue<Vector2>();
+        animator.SetBool("IsMoving", true);
     }
 
-    private void OnCollisionEnter(Collision collision)
-    {   
-        // TODO adjust to fit current game
-
-        //if (rbody.velocity.y < Mathf.Abs(0.01f))
-        //{
-        //    grounded = true;
-        //    isFalling = false;
-        //    if (collision.collider.name != "Plane")
-        //    {
-        //        audio.source.PlayOneShot(audio.sounds[2]);
-        //    }
-        //    else
-        //    {
-        //        audio.source.PlayOneShot(audio.sounds[0]);
-        //    }
-        //}
+    public void OnRotate(InputAction.CallbackContext context)
+    {
+        m_Rotation = context.ReadValue<Vector2>();
     }
 
-    void FixedUpdate()
+    public void OnJump(InputAction.CallbackContext context)
     {
-        if (!gameOver)  // TODO && gameStarted
+        animator.SetBool("IsJumping", true);
+    }
+
+    //public void OnLook(InputAction.CallbackContext context)
+    //{
+    //    m_Look = context.ReadValue<Vector2>();
+    //}
+
+    //public void OnFire(InputAction.CallbackContext context)
+    //{
+    //    switch (context.phase)
+    //    {
+    //        case InputActionPhase.Performed:
+    //            if (context.interaction is SlowTapInteraction)
+    //            {
+    //                StartCoroutine(BurstFire((int)(context.duration * burstSpeed)));
+    //            }
+    //            else
+    //            {
+    //                Fire();
+    //            }
+    //            m_Charging = false;
+    //            break;
+
+    //        case InputActionPhase.Started:
+    //            if (context.interaction is SlowTapInteraction)
+    //                m_Charging = true;
+    //            break;
+
+    //        case InputActionPhase.Canceled:
+    //            m_Charging = false;
+    //            break;
+    //    }
+    //}
+
+    //public void OnGUI()
+    //{
+    //    if (m_Charging)
+    //        GUI.Label(new Rect(100, 100, 200, 100), "Charging...");
+    //}
+
+    public void Update()
+    {
+        // Update orientation first, then move. Otherwise move orientation will lag
+        // behind by one frame.
+        //Look(m_Look);
+        Move(m_Move);
+        Rotate(m_Rotation);
+        //Jump();
+    }
+
+    private void Move(Vector2 direction)
+    {
+        if (direction.sqrMagnitude < 0.01)
         {
-            Vector3 tVel = Vector3.zero;
-            float tempY = rbody.velocity.y;
+            animator.SetBool("IsMoving", false);
+            return;
+        }
 
-            if (Input.GetKey(KeyCode.LeftShift) && isMoving && stamina != 0)
-            {
-                isRunning = true;
-                currentSpeed = runSpeed;
-                animator.SetBool("IsRunning", true);
-            }
-            else
-            {
-                isRunning = false;
-                currentSpeed = walkSpeed;
-                animator.SetBool("IsRunning", false);
-            }
-
-            if (Input.GetKey(KeyCode.W))
-            {
-                tVel += transform.forward;
-            }
-            if (Input.GetKey(KeyCode.S))
-            {
-                tVel -= transform.forward;
-            }
-
-            //if (Input.GetKey(KeyCode.W))
-            //{
-            //    tVel += transform.forward;
-            //    isMoving = true;
-            //    if (Input.GetKey(KeyCode.A))
-            //    {
-            //        transform.rotation = Quaternion.Euler(rotNW);
-            //    }
-            //    else if (Input.GetKey(KeyCode.D))
-            //    {
-            //        transform.rotation = Quaternion.Euler(rotNE);
-            //    }
-            //    else
-            //    {
-            //        transform.rotation = Quaternion.Euler(rotN);
-            //    }
-            //}
-            //if (Input.GetKey(KeyCode.S))
-            //{
-            //    tVel += transform.forward;
-            //    isMoving = true;
-            //    if (Input.GetKey(KeyCode.A))
-            //    {
-            //        transform.rotation = Quaternion.Euler(rotSW);
-            //    }
-            //    else if (Input.GetKey(KeyCode.D))
-            //    {
-            //        transform.rotation = Quaternion.Euler(rotSE);
-            //    }
-            //    else
-            //    {
-            //        transform.rotation = Quaternion.Euler(rotS);
-            //    }
-            //}
-            //if (Input.GetKey(KeyCode.A))
-            //{
-            //    isMoving = true;
-            //    if (!Input.GetKey(KeyCode.W) && !Input.GetKey(KeyCode.S))
-            //    {
-            //        tVel += transform.forward;
-            //        transform.rotation = Quaternion.Euler(rotW);
-            //    }
-            //}
-            //if (Input.GetKey(KeyCode.D))
-            //{
-            //    isMoving = true;
-            //    if (!Input.GetKey(KeyCode.W) && !Input.GetKey(KeyCode.S))
-            //    {
-            //        tVel += transform.forward;
-            //        transform.rotation = Quaternion.Euler(rotE);
-            //    }
-            //}
-            //if (Input.GetKey(KeyCode.Space) && grounded)
-            //{
-            //    isMoving = false;
-            //    rbody.AddForce(transform.up * jumpHeight, ForceMode.Impulse);
-            //    grounded = false;
-            //}
-
-            if (!Input.GetKey(KeyCode.W) && !Input.GetKey(KeyCode.A) && !Input.GetKey(KeyCode.S) && !Input.GetKey(KeyCode.D))
-            {
-                isMoving = false;
-            }
-
-
-            if (isMoving)
-            {
-                animator.SetBool("IsWalking", true);
-            }
-            else
-            {
-                animator.SetBool("IsWalking", false);
-            }
-
-            if (rbody.velocity.y < 0)
-            {
-                isFalling = true;
-            }
-
-            tVel = tVel.normalized * currentSpeed;
-            tVel.y = tempY;
-            rbody.velocity = tVel;
+        var scaledMoveSpeed = moveSpeed * Time.deltaTime;
+        var move = Quaternion.Euler(0, transform.eulerAngles.y, 0) * new Vector3(direction.x, 0, direction.y);
+        transform.position += move * scaledMoveSpeed;
+    }
+    
+    private void Rotate(Vector2 direction)
+    {
+        if (direction.sqrMagnitude < 0.01)
+        {
+            return;
+        }
+        m_Rotation.y = storedRot;
+        if(direction.x == 1)
+        {
+            m_Rotation.y += Time.deltaTime * rotateSpeed;
         }
         else
         {
-            rbody.velocity = new Vector3(0, 0);
-            animator.SetBool("IsWalking", false);
-            animator.SetBool("IsRunning", false);
-            isMoving = false;
+            m_Rotation.y += Time.deltaTime * rotateSpeed * -1;
         }
+
+        Vector2 tempVec = new Vector2(0.0f, m_Rotation.y);
+        transform.localEulerAngles = tempVec;
+        storedRot = m_Rotation.y;
     }
 
-    private void Update()
+    private void Jump(Vector2 direction)
     {
-        if (!gameOver)
+        if (direction.sqrMagnitude < 0.01)
         {
-            // TODO win state/scene loader here if needed
+            animator.SetBool("IsJumping", false);
+            return;
+        }
 
-            timer += Time.deltaTime;
-
-            if (isRunning)
-            {
-                stamina -= Time.deltaTime;
-                if (stamina < 0)
-                {
-                    stamina = 0;
-                }
-            }
-            else
-            {
-                if (!Input.GetKey(KeyCode.LeftShift))
-                {
-                    stamina += Time.deltaTime;
-                    if (stamina > staminaMax)
-                    {
-                        stamina = staminaMax;
-                    }
-                }
-            }
+        if (!hasJumped)
+        {
+            var scaledMoveSpeed = jumpHeight * Time.deltaTime;
+            // For simplicity's sake, we just keep movement in a single plane here. Rotate
+            // direction according to world Y rotation of player.
+            var move = Quaternion.Euler(0, transform.eulerAngles.y, 0) * Vector3.up;
+            transform.position += move * scaledMoveSpeed;
+            hasJumped = true;
         }
     }
+
+    //private void Look(Vector2 rotate)
+    //{
+    //    if (rotate.sqrMagnitude < 0.01)
+    //        return;
+    //    var scaledRotateSpeed = rotateSpeed * Time.deltaTime;
+    //    m_Rotation.y += rotate.x * scaledRotateSpeed;
+    //    m_Rotation.x = Mathf.Clamp(m_Rotation.x - rotate.y * scaledRotateSpeed, -89, 89);
+    //    transform.localEulerAngles = m_Rotation;
+    //}
+
+    //private IEnumerator BurstFire(int burstAmount)
+    //{
+    //    for (var i = 0; i < burstAmount; ++i)
+    //    {
+    //        Fire();
+    //        yield return new WaitForSeconds(0.1f);
+    //    }
+    //}
+
+    //private void Fire()
+    //{
+    //    var transform = this.transform;
+    //    var newProjectile = Instantiate(projectile);
+    //    newProjectile.transform.position = transform.position + transform.forward * 0.6f;
+    //    newProjectile.transform.rotation = transform.rotation;
+    //    const int size = 1;
+    //    newProjectile.transform.localScale *= size;
+    //    newProjectile.GetComponent<Rigidbody>().mass = Mathf.Pow(size, 3);
+    //    newProjectile.GetComponent<Rigidbody>().AddForce(transform.forward * 20f, ForceMode.Impulse);
+    //    newProjectile.GetComponent<MeshRenderer>().material.color =
+    //        new Color(Random.value, Random.value, Random.value, 1.0f);
+    //}
 }
